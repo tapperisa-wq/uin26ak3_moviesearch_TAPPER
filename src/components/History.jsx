06aecbe808d7ee10fb54477534dd58dad56b6e25
tapperisa-wq <tapperisa@hotmail.com>
2026-03-06 12:24:39 +0100

    .

---
+++ src/components/History.jsx
@@ -0,0 +1,12 @@
+export default function History({history, setSearch}){
+    
+    const handleChange= (e) => {
+        setSearch(e.target.value)
+    }
+
+    return(
+         <select onChange={handleChange}>
+            {history?.map((item, i) => <option key={i} value={item}>{item}</option>)}
+         </select>
+    )
+}
---
+++ src/pages/Home.jsx
@@ -0,0 +1,65 @@
+import { useEffect, useState } from "react"
+import History from "../components/History"
+
+export default function Home(){
+
+    const [search, setSearch] = useState()
+    const storedHistory = localStorage.getItem("search")
+    const [focused, setFocused] = useState(false)
+
+    const [history, setHistory] = useState(storedHistory ? JSON.parse(storedHistory) : [])
+
+    console.log("Denne kommer fra storage", storedHistory)
+
+    const baseUrl = `http://www.omdbapi.com/?s=${search}&apikey=`
+    const apiKey = import.meta.env.VITE_APP_API_KEY
+
+    useEffect(()=>{
+        localStorage.setItem("search", JSON.stringify(history))
+    },[history])
+
+    const getMovies = async() => {
+        try{
+            const response = await fetch(`${baseUrl}${apiKey}`)
+            const data = await response.json()
+            console.log(data)
+        }
+
+        catch(err){
+            console.error(err);
+        }
+    }
+
+    const handleChange = (e) => {
+        setSearch(e.target.value)
+    }
+
+    const handleSubmit = (e)=>{
+        e.preventDefault()
+        e.target.reset()
+
+        setHistory((prev) => [...prev, search])
+
+    }
+    console.log(history)
+
+    return(
+        <main>
+            <h1>Forside</h1>
+            <form onSubmit={handleSubmit} >
+                <label>
+                    Søk etter film
+                    <input type="search" placeholder="Harry Potter..." onChange={handleChange} onFocus={()=> setFocused(true)} /*onBlur={()=>setFocused(false)}*/></input>
+                </label>
+            {focused ? <History history = {history} setSearch={setSearch} /> : null}
+            <button onClick={getMovies}>Søk</button>
+            </form>
+        </main>
+    )
+}
+
+//søkefelt
+//søkefelt tar fetch 
+
+//funksjon tøm logg
+//local storage remove
